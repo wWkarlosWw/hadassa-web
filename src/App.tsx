4d8068493f46153f07dd268193e7f4523b1a2a80
wWkarlosWw2 <karlos.batista@q-minex.com>
2026-05-12 11:33:31 -0400

    feat: configure AuthProvider, update project assets, and refactor routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import { AppRouter } from "./router/AppRouter";
-import "./styles/globals.css";
+import { AppRouter } from './router/AppRouter'
+import { AuthProvider } from '@/context'
+import './styles/globals.css'
 
 export function App() {
-  return <AppRouter />;
+  return (
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
+  )
 }
 
-export default App;
+export default App
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,15 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { ROUTES } from '@/shared/constants'
-import { HomePage, DashboardPage, ProfilePage } from '@/pages'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ROUTES } from "@/shared/constants";
 
 export function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={ROUTES.HOME} element={<HomePage />} />
-        <Route path={ROUTES.DASHBOARD} element={<DashboardPage />} />
-        <Route path={ROUTES.PROFILE} element={<ProfilePage />} />
+        {/* Rutas públicas */}
+        {/* <Route path={ROUTES.HOME} element={<HomePage />} />
+        <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+        <Route path={ROUTES.REGISTER} element={<RegisterPage />} /> */}
+
+        {/* Rutas protegidas */}
+        <Route
+          path={ROUTES.DASHBOARD}
+          element={
+            // <ProtectedRoute>
+            //   <DashboardPage />
+            // </ProtectedRoute>
+          }
+        />
+        <Route
+          path={ROUTES.PROFILE}
+          element={
+            // <ProtectedRoute>
+            //   <ProfilePage />
+            // </ProtectedRoute>
+          }
+        />
+
+        {/* 404 */}
+        {/* <Route path="*" element={<HomePage />} /> */}
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
