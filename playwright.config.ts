91e65be3efb0f7612d8fa92a05ad2ad308fa6594
wWkarlosWw <batistaleitecardozokarlos@gmail.com>
2026-06-16 21:47:48 -0400

    feat: cambio de puerto para jenkins

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: "html",
   use: {
-    baseURL: 'http://localhost:5173',
-    trace: 'on-first-retry',
+    baseURL: "http://localhost:5173",
+    trace: "on-first-retry",
   },
   webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:5173',
+    command: "npm run dev -- --port 5174",
+    url: "http://localhost:5174",
     reuseExistingServer: !process.env.CI,
     env: {
-      VITE_API_URL: '',
+      VITE_API_URL: "",
     },
   },
 });
